fe718148f6e3bef4278eb6948ac90c114b496734
MustafaBioS <mustaafahany@gmail.com>
2025-12-09 15:12:58 +0200

    ADD - Main Menu

--- Scripts/player.gd
+++ Scripts/player.gd
@@ -1,5 +1,5 @@
 extends CharacterBody2D
-
+@onready var score = $"../Score"
 
 const SPEED = 500.0
 const JUMP_VELOCITY = -400.0
@@ -9,6 +9,7 @@ func player():
 
 func _process(delta: float) -> void:
 	print(State.score)
+	score.text = str(State.score)
 	
 func _physics_process(delta: float) -> void:
 	var direction := Input.get_axis("a", "d")
